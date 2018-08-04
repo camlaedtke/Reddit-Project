eedab3f4bb244c5ea7bd3067be54f0a289b0f531
Cameron Laedtke <cameronlaedtke@Camerons-MacBook-Pro.local>
2018-08-04 17:25:34 -0500

    tweaks

--- sentiment-analysis/Ensemble.py
+++ sentiment-analysis/Ensemble.py
@@ -3,26 +3,12 @@ from .CorpusAnalysis import CorpusAnalyser
 from .RedditScraper import ScrapeReddit
 from .RedditSentiment import SentimentAnalyser
 
-sub = ScrapeReddit(directory="/Users/cameronlaedtke/PycharmProjects/MLPractice/RedditNLP/data/")
-
-sub.set_subreddit("funny")
-sub.get_posts()
-sub.get_comments()
-
-sent = SentimentAnalyser(directory="/Users/cameronlaedtke/PycharmProjects/MLPractice/RedditNLP/data/",
-                          subreddit="The_Mueller")
-
-sent.run_sentiment_analysis()
-sent.word_distribution('politics/negative_list.txt')
-sent.run_topical_analysis("Kushner")
-sent.stacked_bar(group="topics")
-sent.clear_files()
-
 class Assembler:
 
     def __init__(self, directory):
         self.directory = directory
         self.scraper = ScrapeReddit(directory)
+        self.sentiment = SentimentAnalyser(directory)
         self.corpus = CorpusCreator(directory)
         self.model = CorpusAnalyser(directory)
 
@@ -34,8 +20,8 @@ class Assembler:
         self.corpus.tfidf_vectorize(max_features=max_features)
         self.corpus.normalize()
 
-    def reduce(self, method, data, parameter):
-        self.model.load_array(data)
+    def reduce_dimensions(self, method, data, parameter):
+        self.model.load_data(data)
         if method.lower() == "t-sne":
             self.model.t_sne(perplexity=parameter)
         elif method.lower() == "pca":
@@ -67,8 +53,8 @@ s.load(file_name="politics/headlines.csv")
 s.load(file_name="The_Mueller/headlines.csv")
 
 s.prep(max_features=2000)
-s.reduce(method="pca", data=corpus, parameter=30)
-s.reduce(method="t-sne", data=corpus, parameter=36)
+s.reduce_dimensions(method="pca", data=s.corpus, parameter=30)
+s.reduce_dimensions(method="t-sne", data=s.corpus, parameter=36)
 s.cluster(method="kmeans", clusters=12)
 s.score(method="silhouette")
 s.show()
